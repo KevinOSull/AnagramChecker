deef7a54ae886ef4aaff2726b44ad35cbf2066df
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2026-05-21 12:47:49 +0100

    added main method

---
+++ Anagram.java
@@ -0,0 +1,26 @@
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.InputMismatchException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
+import java.util.function.Supplier;
+import java.util.LinkedHashMap;
+import javax.swing.*;
+public class Anagram extends JFrame{
+
+
+    public Anagram(){
+
+    }
+    
+}
