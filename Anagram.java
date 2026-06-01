703000592a7413088b54431b62e82b79e6595a5f
KevinOSull <kosullivan3336@gmail.com>
2026-06-01 23:18:47 +0100

    fixed up buttons and labels

--- Anagram.java
+++ Anagram.java
@@ -7,12 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.InputMismatchException;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.Timer;
+
 import javax.swing.border.EmptyBorder;
 import java.util.function.Supplier;
 import java.util.LinkedHashMap;
@@ -29,7 +24,7 @@ public class Anagram extends JFrame{
     private JFrame frame = new JFrame();
     private static final Random RAND = new Random();
 
-    private final int X = 200;
+    private final int X = 53;
     private final int Y = 11;
     private final int WIDTH = 392;
     private final int HEIGHT = 42;
@@ -38,6 +33,7 @@ public class Anagram extends JFrame{
 
     private JButton submitButton;
     private JButton resetButton;
+    private JButton exitButton;
 
     private JTextField wordOneTextField;
     private JTextField wordTwoTextField;
@@ -45,50 +41,72 @@ public class Anagram extends JFrame{
     private JLabel wordTwoLabel;
     private JLabel resultLabelText;
     private JLabel errorMessagesLabelText;
-    private JLabel messageOneLbl;
-    private JLabel messageTwoLbl;
     private JLabel headerLabel;
+    private JLabel resultLabel;
+    private JLabel errorMessageLabel;
 
     private Color[] colors = new Color[]{Color.RED,Color.black,Color.green,Color.blue};
 
     public Anagram(){
+        getContentPane().setLayout(null);
         headerLabel = new JLabel("ANAGRAM CHECKER");
-        headerLabel.setFont(new Font("Serif",Font.BOLD,30));
+        headerLabel.setFont(new Font("Tahoma",Font.BOLD,16));
         result = getRandomColor();
         headerLabel.setForeground(setColor());
-         submitButton = new JButton("Submit");
-        resetButton = new JButton("Reset");
-        wordOneLabel = new JLabel("Word One:");
-        wordTwoLabel = new JLabel("Word Two:");
-        resultLabelText = new JLabel("Result:");
-        messageOneLbl = new JLabel("RESULT GOES HERE");
-        messageTwoLbl = new JLabel("ERRORS GO HERE");
-        messageOneLbl.setBounds(21,212,566,14);
-        messageTwoLbl.setBounds(21,250,566,14);
-        errorMessagesLabelText = new JLabel("Error Messages:");
-        resultLabelText.setBounds(21,198,79,14);
-        errorMessagesLabelText.setBounds(21,237,101,14);
-        wordOneLabel.setBounds(93,79,73,14);
-        wordTwoLabel.setBounds(93,128,73,14);
-        wordOneTextField = new JTextField();
-        wordTwoTextField = new JTextField();
-        wordOneTextField.setBounds(179,76,188,20);
-        wordTwoTextField.setBounds(179,125,188,20);
+        headerLabel.setOpaque(true);
+        headerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        headerLabel.setBackground(new Color(0, 128, 255));
         headerLabel.setBounds(X,Y,WIDTH,HEIGHT);
+
+        submitButton = new JButton("Submit");
+        resetButton = new JButton("Reset");
+        exitButton = new JButton("Exit");
+
+        wordOneLabel = new JLabel("Word One:");
+        wordOneLabel.setBounds(93,79,73,14);
+        wordOneLabel.setForeground(Color.red);
+        
+        wordTwoLabel = new JLabel("Word Two:");
+        wordTwoLabel.setBounds(93,128,73,14);
+        wordTwoLabel.setForeground(Color.red);
+
+        resultLabelText = new JLabel("Result:");
+        resultLabelText.setBounds(21,198,79,14);
+        resultLabelText.setForeground(Color.red);
+
+        errorMessagesLabelText = new JLabel("Error Messages:");
+        errorMessagesLabelText.setBounds(21,237,101,14);
+        errorMessagesLabelText.setForeground(Color.red);
+        
+        resultLabel = new JLabel("result go here");
+        resultLabel.setBounds(21,212,566,14);
+
+        errorMessageLabel = new JLabel("error messages go here");
+        errorMessageLabel.setBounds(21,250,566,14);
+
+        wordOneTextField = new JTextField();
+        wordOneTextField.setBounds(179,76,188,20);
+
+        wordTwoTextField = new JTextField();
+        wordTwoTextField.setBounds(179,125,188,20);
+        
         submitButton.setBounds(179,156,89,23);
         resetButton.setBounds(278,156,89,23);
+        exitButton.setBounds(377,156,89,23);
+
         getContentPane().setBackground(Color.yellow);
         add(headerLabel);
         add(submitButton);
         add(resetButton);
+        add(exitButton);
         add(wordOneTextField);
         add(wordTwoTextField);
         add(wordOneLabel);
         add(wordTwoLabel);
         add(resultLabelText);
         add(errorMessagesLabelText);
-        add(messageOneLbl);
-        add(messageTwoLbl);
+        add(resultLabel);
+        add(errorMessageLabel);
 
         submitButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
@@ -99,6 +117,12 @@ public class Anagram extends JFrame{
         resetButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 reset();
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
             }
         });
 
@@ -113,7 +137,18 @@ public class Anagram extends JFrame{
             dispose();
         }
 
+        if(isInputEmpty(wordOneTextField).get() || isInputEmpty(wordTwoTextField).get()){
+            errorMessages(userWordOne,userWordTwo);
+        }
 
+        boolean hasErrors = isLengthOfWordsMismatch(wordOneTextField,wordTwoTextField).get();
+        if(hasErrors){
+            errorMessages(userWordOne,userWordTwo);
+        }
+        caseWordOne = checkCaseOfWord(userWordOne);
+        caseWordTwo = checkCaseOfWord(userWordTwo);
+        processWordsAndArrays(firstWordArray,secondWordArray,caseWordOne,caseWordTwo);
+        checkWinner(userWordOne,userWordTwo,firstWordArray,secondWordArray);
     }
 
     private void checkWinner(String wordOne,String wordTwo,int[] wordOneArray,int[] wordTwoArray){
@@ -170,14 +205,16 @@ public class Anagram extends JFrame{
         return "";
     }
 
-    
-
     private void reset(){
         
     }
 
+    private void exitButton(){
+
+    }
+
     private boolean isExit(){
-        return true;
+        return !userWordOne.equalsIgnoreCase("quit");
     }
 
     private int getRandomColor(){
